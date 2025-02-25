9066763bd8173235b333a8f564caa23e773e14ef
Saadat Antor <76962594+Saadat-Antor@users.noreply.github.com>
2025-02-25 10:06:03 +0600

    Update procedure_load_silver.sql

--- scripts/silver/procedure_load_silver.sql
+++ scripts/silver/procedure_load_silver.sql
@@ -19,12 +19,12 @@ TRIM(cst_lastname) AS cst_lastname,
 CASE WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
 	 WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
 	 ELSE 'n/a' 
-END cst_marital_status,
+END AS cst_marital_status,
 -- incresing verbocity on cst_gndr, ensuring standardization
 CASE WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
 	 WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
 	 ELSE 'n/a'
-END cst_gndr,
+END AS cst_gndr,
 cst_create_date
 FROM (SELECT
 *,
@@ -61,10 +61,49 @@ CASE UPPER(TRIM(prd_line))
 	 WHEN 'R' THEN 'Road'
 	 WHEN 'S' THEN 'Other sales'
 	 WHEN 'T' THEN 'Touring'
-END prd_line,
+END AS prd_line,
 -- data type casting and handling date discrepencies
 CAST(prd_start_dt AS DATE) AS prd_start_dt,
 CAST(LEAD(prd_start_dt) OVER(PARTITION BY prd_key ORDER BY prd_start_dt) - 1 AS DATE) AS prd_end_dt
 FROM bronze.crm_prd_info
 
-SELECT * FROM silver.crm_prd_info
+
+-- =============== silver.crm_sales_details ===============
+-- inserting data into the table
+INSERT INTO silver.crm_sales_details(
+	sls_ord_num,
+	sls_prd_key,
+	sls_cust_id,
+	sls_order_dt,
+	sls_ship_dt,
+	sls_due_dt,
+	sls_sales,
+	sls_quantity,
+	sls_price)
+
+SELECT
+sls_ord_num,
+sls_prd_key,
+sls_cust_id,
+-- checking sls_order_dt and double-casting data types
+CASE WHEN sls_order_dt = 0 OR LEN(sls_order_dt) != 8 THEN NULL
+	 ELSE CAST(CAST(sls_order_dt AS NVARCHAR) AS DATE)
+END AS sls_order_dt,
+-- checking sls_ship_dt and double-casting data types
+CASE WHEN sls_ship_dt = 0 OR LEN(sls_ship_dt) != 8 THEN NULL
+	 ELSE CAST(CAST(sls_ship_dt AS NVARCHAR) AS DATE)
+END AS sls_ship_dt,
+-- checking sls_due_dt and double-casting data types
+CASE WHEN sls_due_dt = 0 OR LEN(sls_due_dt) != 8 THEN NULL
+	 ELSE CAST(CAST(sls_due_dt AS NVARCHAR) AS DATE)
+END AS sls_due_dt,
+CASE WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price)
+		THEN sls_quantity * ABS(sls_price)
+	 ELSE sls_sales
+END AS sls_sales,
+sls_quantity,
+CASE WHEN sls_price IS NULL OR sls_price <= 0
+		THEN sls_sales/NULLIF(sls_quantity,0)
+	 ELSE sls_price
+END AS sls_price
+FROM bronze.crm_sales_details
